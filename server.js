1ec30d0c091fa3d6c9070d24af80cfd48fb75f4c
manisbindra <maninder.bindra@gmail.com>
2018-09-26 17:18:52 +0530

    modified app message

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var http = require('http');
-var port = process.env.port ||  process.env.PORT ||1337;
+var port = process.env.port ||  process.env.PORT || 1337;
+var appEnvironment = process.env.APP_ENV || " local node";
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello - World\n');
+    res.end('App running on : ' + appEnvironment + '\n');
 }).listen(port);
 console.log("running on port " + port);
