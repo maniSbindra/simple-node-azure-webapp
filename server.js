e908c663ef44fe9952d55adce927ac6c8c22112b
manisbindra <maninder.bindra@gmail.com>
2018-09-26 17:27:47 +0530

    change message to hello world

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var port = process.env.port ||  process.env.PORT || 1337;
 var appEnvironment = process.env.APP_ENV || " local node";
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('App running on : ' + appEnvironment + '\n');
+    res.end('Hello - World\n');
 }).listen(port);
 console.log("running on port " + port);
