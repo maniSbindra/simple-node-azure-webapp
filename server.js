dacad7cf15e54a62c404809090fbf8157cbb6f70
Mani Bindra <maniSbindra@users.noreply.github.com>
2018-10-23 12:30:26 +1100

    Update server.js
    modified message

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var port = process.env.port ||  process.env.PORT || 1337;
 var appEnvironment = process.env.APP_ENV || " local node";
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello Pune');
+    res.end('Hello');
 }).listen(port);
 console.log("running on port " + port);
